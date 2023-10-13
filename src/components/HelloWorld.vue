4a9e001d34c259ece5f0c7b15197b6f16ed5811e
Camila-Negron <carla.negron@ucb.edu.bo>
2023-10-12 20:58:54 -0400

    pais error

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,28 +4,20 @@
     <label for="pais">País:</label>
     <select id="pais" v-model="paisSeleccionado" @change="listCities">
       <option value="todos">Selecciona un país</option>
-      <!-- <option value="todos">Mostrar todos los países</option> -->
-
+      <option v-for="country in countries" :key="country.id" :value="country.nombre">{{ country.nombre }}</option>
     </select>
 
-    <div v-if="paisSeleccionado === 'todos'">
-      <h2>Lista de todos los países:</h2>
-      <ul>
-        <li v-for="country in countries" :key="country.id">{{ country.nombre }}</li>
-      </ul>
-    </div>
   </div>
 </template>
 
+
 <script>
 import axios from 'axios';
-//axios.defaults.baseURL = 'http://localhost:9999/api/v1/country';
+/* axios.defaults.baseURL = 'http://localhost:9999/api/v1'; */
 
 export default {
   data() {
     return {
-      titulo: 'Selección de ciudades',
-      paisSeleccionado: '',
       countries: []
     };
   },
@@ -39,7 +31,7 @@ export default {
           this.countries = response.data;
         })
           .catch(error => {
-            console.error('Error al obtener la lista de tareas:', error);
+            console.error('Error al obtener la lista de paises:', error);
           });
     },  
   },
